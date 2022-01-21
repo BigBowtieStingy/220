eb03d4745e2df374ce20607900a3f79fd15f7c44
jamesaw009 <jamesaw@g.cofc.edu>
2022-01-21 09:35:24 -0500

    Completed Lab 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,45 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+Name: Alex James
+hw1.py
+Problem: This program solves some conversion, cost calculation, and area/volume problems.
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Enter the player's total number of shots: "))
+    shots_made = eval(input("Enter the player's number of successful shots: "))
+    shots_percent = (shots_made / total_shots) * 100
+    print("Shooting Percentage =", shots_percent, "%")
 
 
 def coffee():
-    pass
+    pounds_requested = eval(input("How many pounds of coffee would you like?"))
+    cost = (pounds_requested * 10.50) + (pounds_requested * .86) + 1.50
+    print("Your total cost is", cost)
 
 
 def kilometers_to_miles():
-    pass
+    # 1 mile = 1.61 kilometers.
+    kilometers = eval(input("How many kilometers did you travel?"))
+    miles = kilometers / 1.61
+    print("That's", miles, "miles!")
 
 
 if __name__ == '__main__':
