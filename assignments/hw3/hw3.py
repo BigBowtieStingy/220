d533f697fc0ac9440ca11159bda405ba43ffe090
jamesaw009 <jamesaw@g.cofc.edu>
2022-02-04 21:40:23 -0500

    Completed Homework 3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,60 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+Name: Alex James
+hw3.py
+Problem: Create programs that require the use of "For" loops.
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def average():
-    pass
+    grades_to_average = eval(input("How many grades do you want to average?"))
+    grade_total = 0
+    for i in range(grades_to_average):
+        grade_total = eval(input("Enter grade")) + grade_total
+    print("The average is", (grade_total/grades_to_average))
 
 
 def tip_jar():
-    pass
+    donation_total = 0
+    for i in range(5):
+        donation_total = eval(input("How much would you like to tip?")) + donation_total
+    print("Total tips:", donation_total)
 
 
 def newton():
-    pass
+    num_to_square_root = eval(input("What number do you want to square root?"))
+    approximation_times = eval(input("How many times should we improve the approximation?"))
+    approximated_num = num_to_square_root
+    for i in range(approximation_times):
+        approximated_num = ((num_to_square_root / approximated_num) + approximated_num)/2
+    print("The square root is approximately", approximated_num)
 
 
 def sequence():
-    pass
+    sequence_length = eval(input("How many numbers do you want in the sequence?"))
+    sequence_number = 1
+    sequence_adder = 0
+    for i in range(1, (sequence_length + 1)):
+        sequence_number = (sequence_adder * 2)
+        sequence_adder = int(i / 2)
+        print(sequence_number + 1, end=" ")
 
 
 def pi():
-    pass
+    terms_in_series = eval(input("How many terms in series?"))
+    denom = 1
+    denom_adder = 0
+    numerator = 1
+    numer_adder = 0
+    product = 1
+    for i in range(1, terms_in_series + 1):
+        denom = (denom_adder * 2)
+        denom_adder = int((i + 1) / 2)
+        numerator = (numer_adder * 2)
+        numer_adder = int(i / 2)
+        product = ((numerator + 2)/(denom + 1)) * product
+    print(product * 2)
 
 
 if __name__ == '__main__':
