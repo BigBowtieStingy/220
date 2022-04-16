94e7f737fd36c51d2d743a46bc681287b96186cf
Alex James <97563276+jamesaw009@users.noreply.github.com>
2022-04-15 23:12:47 -0400

    Completed Homework 11 (sales_person/sales_force)

---
+++ assignments/hw11/sales_force.py
@@ -0,0 +1,71 @@
+"""
+Name: Alex James
+sales_force.py
+    Problem: Create a SalesForce class.
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+from sales_person import SalesPerson
+
+
+class SalesForce:
+    def __init__(self):
+        self.sales_people = []
+
+    def add_data(self, file_name):
+        sales_list = open(file_name, 'r')
+        count = 0
+        for line in sales_list.readlines():
+            line = line.replace(",", "")
+            line = line.replace("\n", "")
+            split_line = line.split(" ")
+            current_sales_person = (SalesPerson(eval(split_line[0]), split_line[1] + " " + split_line[2]))
+            for i in range(3, len(split_line)):
+                current_sales_person.enter_sale(eval(split_line[i]))
+            self.sales_people.append(current_sales_person)
+            count += 1
+        sales_list.close()
+
+    def quota_report(self, quota):
+        people_list = []
+        for sales_person in self.sales_people:
+            person_list = [sales_person.get_id(), sales_person.get_name(),
+                           sales_person.total_sales(), sales_person.met_quota(quota)]
+            people_list.append(person_list)
+        return people_list
+
+    def top_seller(self):
+        people_list = []
+        top_seller = self.sales_people[0]
+        count = 1
+        while count < len(self.sales_people):
+            test_person = self.sales_people[count]
+            if top_seller.total_sales() > test_person.total_sales():
+                count += 1
+            elif top_seller.total_sales() < test_person.total_sales():
+                top_seller = test_person
+                count += 1
+            else:
+                people_list.append(test_person)
+                count += 1
+        people_list.append(top_seller)
+        # In the event a value ties, the code below removes values that tied lower than the highest value
+        for final_person in people_list:
+            if final_person.total_sales() > people_list[-1].total_sales():
+                people_list.remove(final_person)
+        return people_list
+
+    def individual_sales(self, employee_id):
+        for sales_person in self.sales_people:
+            found = employee_id == sales_person.get_id()
+            if found:
+                return sales_person
+        return None
+
+    def get_sale_frequencies(self):
+        sale_dict = {}
+        for sales_person in self.sales_people:
+            for sale in sales_person.get_sales():
+                sale_dict[sale] = sale_dict.get(sale, 0) + 1
+        return sale_dict
---
+++ assignments/hw11/sales_person.py
@@ -0,0 +1,48 @@
+"""
+Name: Alex James
+sales_person.py
+Problem: Create a Salesperson class.
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+class SalesPerson:
+    def __init__(self, employee_id, name):
+        self.employee_id = employee_id
+        self.name = name
+        self.sales = []
+
+    def get_id(self):
+        return self.employee_id
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def enter_sale(self, sale):
+        self.sales.append(sale)
+
+    def total_sales(self):
+        sale_sum = 0
+        for sale in self.sales:
+            sale_sum += sale
+        return sale_sum
+
+    def get_sales(self):
+        return self.sales
+
+    def met_quota(self, quota):
+        return self.total_sales() >= quota
+
+    def compare_to(self, other):
+        if self.total_sales() > other.total_sales():
+            return 1
+        if self.total_sales() < other.total_sales():
+            return -1
+        return 0
+
+    def __str__(self):
+        return '{}-{}: {}'.format(self.employee_id, self.name, self.total_sales())
